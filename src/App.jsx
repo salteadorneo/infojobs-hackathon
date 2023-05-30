187146823a78d632f9d6b56c8e59ca97f92de249
Cristian Adán <hola@salteadorneo.dev>
2023-05-30 02:04:19 +0200

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Nav from './components/Nav'
 import Home from './pages/Home'
 import Province from './pages/Province'
 import Search from './components/Search'
+import { GitHub } from './components/GitHub'
 
 function App () {
   return (
@@ -18,6 +19,12 @@ function App () {
       <main className='max-w-[1280px] mx-auto p-6'>
         <Route path='/' component={Home} />
         <Route path='/:province' component={Province} />
+        <p className='flex items-center justify-center gap-2 text-xs mt-4'>
+          Para la demo, se listan hasta 200 ofertas por provincia.
+        </p>
+        <p className='flex items-center justify-center gap-2 text-xs mt-4'>
+          Cristian Adán <a href='https://github.com/salteadorneo/infojobs-hackathon' target='_blank' rel='noreferrer' className='flex items-center gap-2 hover:underline'><GitHub /> @salteadorneo/infojobs-hackathon</a>
+        </p>
       </main>
     </>
   )
